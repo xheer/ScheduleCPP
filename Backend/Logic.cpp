ac238f500a096ed7a4549d877cfbf1dbea57425a
DaniMeloni <122004295+DaniMeloni@users.noreply.github.com>
2023-01-18 04:10:06 -0600

    matthew dumb ngl

--- Backend/Logic.cpp
+++ Backend/Logic.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "json.hpp"
+#include <fstream>
+#include <string>
 using json = nlohmann::json;
 
 
 
 int main()
 {
-    std::cout << "Hello World";
+   // //Specifys the length of the charList array
+   // std::string stringList {};
+   // std::ofstream myFile("test.txt");
+   //myFile.open("test.txt");
+   // if (myFile.is_open())
+   // {
+   //     std::cout << "File opened succesfully" << "\n";
+   // }
+   // std::cout << "Enter stuff please" << "\n";
+   // std::getline(std::cin >> std::ws, stringList);
+   // myFile << stringList;
+   // myFile.close();
+   // std::cout << "Operation Complete Matthew's mom has been secured" << std::endl;
+   // return 0;
+    std::string word;
+    std::cin >> word;
+    std::ofstream file("hello.txt");
+    file << word;
+    file.close();
     return 0;
 }
-// matthew bootyhole

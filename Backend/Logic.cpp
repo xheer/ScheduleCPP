5d1f3450f6c4e3b08dbb24000ddf4a4cffd0f545
DaniMeloni <122004295+DaniMeloni@users.noreply.github.com>
2023-01-18 04:19:54 -0600

    matthew wants strings files

--- Backend/Logic.cpp
+++ Backend/Logic.cpp
@@ -8,24 +8,25 @@ using json = nlohmann::json;
 
 int main()
 {
-   // //Specifys the length of the charList array
-   // std::string stringList {};
-   // std::ofstream myFile("test.txt");
-   //myFile.open("test.txt");
-   // if (myFile.is_open())
-   // {
-   //     std::cout << "File opened succesfully" << "\n";
-   // }
-   // std::cout << "Enter stuff please" << "\n";
-   // std::getline(std::cin >> std::ws, stringList);
-   // myFile << stringList;
-   // myFile.close();
-   // std::cout << "Operation Complete Matthew's mom has been secured" << std::endl;
-   // return 0;
-    std::string word;
+    //Specifys the length of the charList array
+    std::string stringList {};
+    std::ofstream myFile;
+    myFile.open("test.txt");
+    if (myFile.is_open())
+    {
+        std::cout << "File opened succesfully" << "\n";
+    }
+    std::cout << "Enter stuff please" << "\n";
+    std::getline(std::cin >> std::ws, stringList);
+    myFile << stringList;
+    myFile.close();
+    std::cout << "Operation Complete Matthew's mom has been secured" << std::endl;
+    return 0;
+    
+    /*std::string word;
     std::cin >> word;
     std::ofstream file("hello.txt");
     file << word;
     file.close();
-    return 0;
+    return 0;*/
 }

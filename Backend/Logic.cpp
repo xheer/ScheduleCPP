6a4365fff5d998fad0ed3134563d0686112b5d87
DaniMeloni <122004295+DaniMeloni@users.noreply.github.com>
2023-01-18 04:35:32 -0600

    file opener thingy

--- Backend/Logic.cpp
+++ Backend/Logic.cpp
@@ -10,23 +10,19 @@ int main()
 {
     //Specifys the length of the charList array
     std::string stringList {};
-    std::ofstream myFile;
+    std::ifstream myFile;
     myFile.open("test.txt");
     if (myFile.is_open())
     {
         std::cout << "File opened succesfully" << "\n";
     }
-    std::cout << "Enter stuff please" << "\n";
-    std::getline(std::cin >> std::ws, stringList);
-    myFile << stringList;
+    /*std::cout << "Enter stuff please" << "\n";*/
+    /*std::getline(std::cin >> std::ws, stringList);*/
+    while (std::getline(myFile, stringList))
+    {
+        std::cout << stringList << "\n" << "\n";
+    }
     myFile.close();
     std::cout << "Operation Complete Matthew's mom has been secured" << std::endl;
     return 0;
-    
-    /*std::string word;
-    std::cin >> word;
-    std::ofstream file("hello.txt");
-    file << word;
-    file.close();
-    return 0;*/
 }
